cc21f6f0833cbaa00fd583f2153154fb47b88eaa
miladevweb <miladevweb@outlook.com>
2024-10-03 11:34:09 -0500

    change image urls

--- src/data.js
+++ src/data.js
@@ -1,42 +1,42 @@
 export const data = [
   {
-    url: 'https://i.pinimg.com/736x/dd/26/2d/dd262d0c8945adafef88113d0691c6b2.jpg',
-    title: 'Wonyoung',
+    url: 'https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage1.webp?alt=media&token=04511d9a-de51-46ea-92e4-e31eea9555fb',
+    title: 'Express',
     description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quae dolor nesciunt ea nihil possimus at cum omnis aliquam eaque!',
   },
   {
-    url: 'https://1409791524.rsc.cdn77.org/data/images/full/597401/ahn-yujin.jpg',
-    title: 'Yujin',
+    url: 'https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage2.webp?alt=media&token=04511d9a-de51-46ea-92e4-e31eea9555fb',
+    title: 'Hono',
     description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quae dolor nesciunt ea nihil possimus at cum omnis aliquam eaque!',
   },
   {
-    url: 'https://lh3.googleusercontent.com/mzTgX77HTYPIW7eyS81rKn9z5EOAyTCWjdz2yfz8sQzbnJbaQhp2w7km2nA0kAX-P8EmFDkQtv59s54OxOHsm6ZRzXtUHiXngKXduluyAyZEip_s=w960-rj-nu-e365',
-    title: 'Leeseo',
+    url: 'https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage3.webp?alt=media&token=16118553-496e-45ae-b6e0-9fdf6b407f5c',
+    title: '.NET',
     description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quae dolor nesciunt ea nihil possimus at cum omnis aliquam eaque!',
   },
   {
-    url: 'https://kpopping.com/documents/c2/4/220225-TWICE-Instagram-Update-Nayeon-documents-1.jpeg?v=c7f9c',
-    title: 'Nayeon',
+    url: 'https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage4.webp?alt=media&token=16118553-496e-45ae-b6e0-9fdf6b407f5c',
+    title: 'TypeScript',
     description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quae dolor nesciunt ea nihil possimus at cum omnis aliquam eaque!',
   },
   {
-    url: 'https://cdn.milenio.com/uploads/media/2022/03/23/los-fans-de-la-manda.jpg',
-    title: 'Mina',
+    url: 'https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage5.webp?alt=media&token=16118553-496e-45ae-b6e0-9fdf6b407f5c',
+    title: 'Angular',
     description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quae dolor nesciunt ea nihil possimus at cum omnis aliquam eaque!',
   },
   {
-    url: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/20220121%E2%80%94Sana_%EC%82%AC%EB%82%98_Campaign_Film%2C_Pearlygates_x_Twice_2022.jpg',
-    title: 'Sana',
+    url: 'https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage6.webp?alt=media&token=16118553-496e-45ae-b6e0-9fdf6b407f5c',
+    title: 'Svelte',
     description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quae dolor nesciunt ea nihil possimus at cum omnis aliquam eaque!',
   },
   {
-    url: 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Twice_Dahyun_Marie_Claire_Korea_2022_2.png',
-    title: 'Dahyun',
+    url: 'https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/test-images%2Fimage7.webp?alt=media&token=16118553-496e-45ae-b6e0-9fdf6b407f5c',
+    title: 'React',
     description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quae dolor nesciunt ea nihil possimus at cum omnis aliquam eaque!',
   },
   {
-    url: 'https://64.media.tumblr.com/f3af506ee0c4dcbaa02e41e0b0292f04/0b700c920eb52fb7-96/s1280x1920/3bc0cfa421463312c0852d06a4a0b12bb9f84454.jpg',
-    title: 'Tzuyu',
+    url: 'https://firebasestorage.googleapis.com/v0/b/portfolio-f1335.appspot.com/o/disney-clone%2Fstar-wars%2Fsw-1.webp?alt=media&token=223b447a-eed4-4cfe-97db-a46c4b07aa24',
+    title: 'Star Wars',
     description: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quae dolor nesciunt ea nihil possimus at cum omnis aliquam eaque!',
   },
 ]
